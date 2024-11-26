b513f0f459d047573da2d449e3aaca0f55f228ed
Lucia-Susanna <lucia_susanna@hotmail.it>
2024-11-26 15:26:29 +0100

    feat: random numbers

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const timer = document.getElementById('timer');
 const start = document.getElementById('start')
+const randomNumbers = document.getElementById('random-numbers')
+
 let counter = 10
 timer.innerHTML = counter;
 
-//creo il conto alla rovescia
-start.addEventListener('click',() =>{
+let pcNumbers = [];
 
+
+console.log(pcNumbers)
+
+start.addEventListener('click',() =>{
+  
+  // 1. creo il conto alla rovescia
   const interval = setInterval (()=>{
    // a ogni intervallo il mio counter si decrementa  
    counter --
@@ -16,6 +23,19 @@ start.addEventListener('click',() =>{
    }
   
   }, 1000)
+
+  // 2. genero 5 numeri casuali da 1 a 50
+  
+  for (let i= 0; i <= 4; i++){
+   pcNumbers.push(Math.ceil(Math.random() * 50));
+   randomNumbers.innerHTML = pcNumbers;
+  }
+
+  start.disabled = true;
+
+
+
+
 })
 
 
